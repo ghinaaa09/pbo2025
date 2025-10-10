364e433932339e073691230c7581b4a06526d01f
Ghina <h1101241015@student.untan.ac.id>
2025-10-10 16:18:33 +0700

    Praktikum 6C - Implementasi Enkapsulasi

---
+++ public/sebelas.php
@@ -0,0 +1,8 @@
+<?php
+use App\Akademik\Dosen;
+require_once 'vendor/autoload.php';
+
+$dian = new Dosen(198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
+$dian->mengajar();
+echo $dian->getNoHp();
+$dian->setNidn("1000121184001");
