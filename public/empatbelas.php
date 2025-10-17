2c9f6c215aa8d10956d8eaefaafd974f2f3f8cbd
Ghina <h1101241015@student.untan.ac.id>
2025-10-17 16:03:00 +0700

    Praktikum 7D - Menggunakan Interface

--- public/empatbelas.php
+++ public/empatbelas.php
@@ -2,7 +2,14 @@
 
 require_once '../vendor/autoload.php';
 
-use App\Akademik\Pegawai;
+use App\Akademik\Dosen;
+use App\Akademik\TenagaKependidikan;
 
-$pegawai = new Pegawai(100, "Pegawai Abstrak", "08111234", "Jl. Antah Berantah");
-$pegawai->cekIn();
+$dosen1 = new Dosen(101, "Budi Hartono", "0812...", "Jl. Cendrawasih No. 10", "198012345");
+$tendik1 = new TenagaKependidikan(201, "Ani Suryani", "0815...", "Jl. Merpati No. 5", 5000000);
+$tunjanganDosen = $dosen1->hitungTunjanganKinerja();
+$tunjanganTendik = $tendik1->hitungTunjanganKinerja();
+
+echo "<h2>Perhitungan Tunjangan Kinerja</h2>";
+echo "Tunjangan Kinerja untuk Dosen ({$dosen1->nama}): Rp " . number_format($tunjanganDosen, 0, ',', '.') . "<br>";
+echo "Tunjangan Kinerja untuk Tenaga Kependidikan ({$tendik1->nama}): Rp " . number_format($tunjanganTendik, 0, ',', '.') . "<br>";
